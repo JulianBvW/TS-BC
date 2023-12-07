46f4d3b58048268f26e00d55f0ea5a904ee7b52f
JulianBvW <julianhendrik.bvw@gmail.com>
2023-12-07 01:03:33 +0100

    Dynamic device set

--- LatentSpaceVPT.py
+++ LatentSpaceVPT.py
@@ -14,12 +14,13 @@ AGENT_RESOLUTION = (128, 128)
 CONTEXT = {'first': torch.tensor([[False]])}
 
 class LatentSpaceVPT:
-    def __init__(self):
+    def __init__(self, device='cuda'):
         self.latents = []  # Python List while training, Numpy array while inference
+        self.device = device
     
     @torch.no_grad()
     def load(self, latents_file='weights/ts_bc/latents_vpt.npy'):  # TODO set device
-        self.latents = torch.from_numpy(np.load(latents_file, allow_pickle=True)).to('cuda')
+        self.latents = torch.from_numpy(np.load(latents_file, allow_pickle=True)).to(self.device)
         print(f'Loaded VPT latent space with {len(self.latents)} latents')
         return self
     
@@ -35,7 +36,7 @@ class LatentSpaceVPT:
         for ts in range(frames.shape[0]):
             resized_frame = cv2.resize(frames[ts], AGENT_RESOLUTION)
             resized_frames[ts] = resized_frame
-        frames = torch.tensor(resized_frames).to('cuda')
+        frames = torch.tensor(resized_frames).to(self.device)
 
         for ts in range(SLIDING_WINDOW_SIZE-1, len(frames)):  # Start at Frame 15 because of MineCLIP needing 16-frame batches
             frame = frames[ts].unsqueeze(0).unsqueeze(0)  # Add 2 extra dimensions for vpt
@@ -46,6 +47,16 @@ class LatentSpaceVPT:
         
         del(frames)
 
+    def get_distances(self, latent):  # TODO look at different norms
+        diffs = self.latents - latent
+        diffs = torch.abs(diffs).sum(1)  # Sum up along the single latents exponential difference to the current latent
+        return diffs
+    
+    def get_distance(self, idx, latent):  # TODO refactor to use `self.distance_function` or something
+        diff = self.latents[idx] - latent
+        diff = torch.abs(diff).sum()
+        return diff
+
     def get_nearest(self, latent): # TODO removed episode_starts
         # TODO assert latents is numpy array
 
@@ -54,17 +65,7 @@ class LatentSpaceVPT:
         nearest_idx = diffs.argmin()#.to('cpu').item() # TODO remove .to('cpu').item()
         return nearest_idx
 
-        episode = 0
-        while episode+1 < len(self.episode_starts) and int(self.episode_starts[episode+1][1]) <= nearest_idx:
-            episode += 1
-        episode_id, episode_start = self.episode_starts[episode]
-        episode_start = int(episode_start)
-
-        print(f'Found nearest in {episode_id} at frame {nearest_idx - episode_start} ({(nearest_idx - episode_start) // 20 // 60}:{((nearest_idx - episode_start) // 20) % 60})')
-
-        return nearest_idx
-
-def load_vpt(model_file='weights/vpt/foundation-model-1x.model', weights_file='weights/vpt/foundation-model-1x.weights'):
+def load_vpt(model_file='weights/vpt/foundation-model-1x.model', weights_file='weights/vpt/foundation-model-1x.weights', device='cuda'):
     agent_parameters = pickle.load(open(model_file, 'rb'))
 
     policy_kwargs = agent_parameters['model']['args']['net']['args']
@@ -74,4 +75,4 @@ def load_vpt(model_file='weights/vpt/foundation-model-1x.model', weights_file='w
     agent = MinecraftAgentPolicy(policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs, action_space=DictType())
     agent.load_state_dict(torch.load(weights_file), strict=False)
 
-    return agent.to('cuda')
+    return agent.to(device)
